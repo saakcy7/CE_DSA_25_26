d2789121bd97baf25264efa99329c3e4bd89f3e5
Sakshi KC <kcsakshi92@gmail.com>
2024-06-19 10:18:40 +0545

    graph.h

---
+++ include/graph.h
@@ -0,0 +1,20 @@
+#include<vector>
+class Graph{
+    private:
+        bool directed;
+        std::vector<std::vector<int>> adjMatrix;
+
+    public:
+        Graph(int numVertices,bool directed=true);
+        void addEdge(int u, int v);
+        void removeEdge(int u, int v);
+        bool isEdge(int u, int v);
+        void printGraph();
+        std::vector<int> getNeighbours(int u);
+        int getNumVertices();
+        int getNumEdges();
+        bool isDirected();
+        bool isEmpty();
+        bool isFull();
+        ~Graph();
+};
